bdcb1419bf715736298caa9c0d4e5846a31b7d48
Boburjon Ravzatov <63533094+Boburjon04@users.noreply.github.com>
2023-05-12 12:31:08 +0500

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,5 @@
+echo "Cloning Repo...."
+git clone https://github.com/Boburjon04/URL-Uploader04
+pip3 install -U -r requirements.txt
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @Bobur_Ravzatov"
+python3 bot.py
